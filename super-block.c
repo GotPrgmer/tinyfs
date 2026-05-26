e5f3a1bb1bb251f12111ec0d297fc4ae9354045d
Gotprgmer <gofiction95@gmail.com>
2026-05-26 11:28:42 +0900

    super-block 수정

--- super-block.c
+++ super-block.c
@@ -1,12 +1,11 @@
 #include <i-node.c>
-#define INODE_NUMBER
-#define MAX_FILES 16
+
 
 typedef struct
 {
 
-    int BLOCK_NUMBER;
-    int INODE_NUMBER;
-    Inode inode_list[MAX_FILES];
+    int total_blocks;
+    int total_inodes;
+
 
 }superBlock;
